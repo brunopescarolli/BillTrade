32631b29e338d3ca6b551d30c25dc97813f5d14d
zock <bruno.pescarolli@outlook.com>
2020-02-21 22:11:36 -0300

    ta funcionando

--- Billie/App/src/main/java/com/example/bill/MainActivity.java
+++ Billie/App/src/main/java/com/example/bill/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.bill;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegistar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegistar = (Button) findViewById(R.id.idBtnRegistrar);
+
+        btnRegistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegister();
+
+            }
+        });
+    }
+    public void openRegister(){
+        Intent intent = new Intent(this, register_activity.class);
+        startActivity(intent);
     }
 }
---
+++ Billie/App/src/main/java/com/example/bill/register_activity.java
@@ -0,0 +1,14 @@
+package com.example.bill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
+}
